c80ae7e2c1c26aec70c7abe145c8b03ff6a88938
ademidun <tomiademidun@gmail.com>
2018-11-19 07:03:58 -0500

    added math games component

---
+++ src/app/math-games/math-games.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-math-games',
+  templateUrl: './math-games.component.html',
+  styleUrls: ['./math-games.component.scss']
+})
+export class MathGamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
